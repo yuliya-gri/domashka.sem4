bb2c18fd2567772e80b76d7a7c7599d767755db3
yuliya-gri <yuliya.grigoreva.1986@list.ru>
2022-12-06 17:52:22 +0400

    commit DZ1

---
+++ DZ1.sem4/Program.cs
@@ -0,0 +1,20 @@
+﻿//напишите цикл, который принимает на вход 2 числа (A и B)
+//и возводит число (А) в натуральную степень (В)
+
+Console.WriteLine("Введите 1 число");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(" Введите 2 число");
+int num2 = Convert.ToInt32(Console.ReadLine());
+
+
+int DegNum(int num1, int num2)
+
+{
+    int x = num1;
+    for(int i = 1; i < num2; i++)
+
+     x *= num1;
+    return x;
+}
+Console.WriteLine($"Значение первого числа ,возведенное в степень,равную второму числу = ");
+Console.WriteLine($"{DegNum(num1,num2)}");
