0f18f5f0f0ec6fc7f2451ca9caeb58fe695aca30
yuliya-gri <yuliya.grigoreva.1986@list.ru>
2022-12-06 18:03:12 +0400

    commit DZ2

---
+++ DZ2.sem4/Program.cs
@@ -0,0 +1,13 @@
+﻿
+//напишите программу, которая на вход принимает число
+//и выдает сумму цифр в числе
+
+Console.WriteLine("Введите число");
+int N = Convert.ToInt32(Console.ReadLine());
+int SummNum(int N)
+{   int num = 0;
+    for(int i = 1; i <= N; i++ ) num += i;
+    return num;
+}
+
+Console.WriteLine($"Сумма всех цифр в числе {N} равна {SummNum(N)} ");
