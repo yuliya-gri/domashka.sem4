d5d11391ef7b8f993b52b12b850d26d21626309a
yuliya-gri <yuliya.grigoreva.1986@list.ru>
2022-12-08 12:25:08 +0400

    commit DZ3

---
+++ DZ.3.sem4/Program.cs
@@ -0,0 +1,21 @@
+﻿int [] FillArray()
+{
+    int [] myArray = new int [8];    
+    for (int i = 0; i < 8; i++)
+    {
+        Console.WriteLine("Введите элемент массива: ");
+        myArray[i] = Convert.ToInt32(Console.ReadLine());
+    }
+    return myArray;
+}
+void PrintArray (int [] myArray)
+{
+    for (int i = 0; i < 8; i++)
+    {
+    Console.Write(myArray[i]);
+    }
+}
+
+int [] myArray = FillArray();
+Console.WriteLine("Вывод массива");
+PrintArray(myArray);
